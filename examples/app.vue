755b68d8a3dbbdbf097127f726f596db2cd0fb1b
hanquanwen <www.799155945@qq.com>
2023-12-20 17:26:52 +0800

    first commit

---
+++ examples/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" :class="{ 'is-component': true }">
+    <div>
+      111
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+
+  methods: {},
+
+  mounted() {},
+};
+</script>
---
+++ packages/main/src/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="will-main">
+    <slot></slot>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'WillMain',
+    componentName: 'WillMain'
+  };
+</script>
